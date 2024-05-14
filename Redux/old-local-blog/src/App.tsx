5e389906293bd4ba5cb080d65688e119e344e305
Le Thuy Vy <lethuyvy321@gmail.com>
2024-05-14 17:47:50 +0700

    CreateReducer And CreateAction in Redux

--- Redux/old-local-blog/src/App.tsx
+++ Redux/old-local-blog/src/App.tsx
@@ -1,13 +1,7 @@
+import Blog from 'pages/blog/Blog'
+
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <Blog></Blog>
 }
 
 export default App
