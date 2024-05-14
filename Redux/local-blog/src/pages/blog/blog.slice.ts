9f452a0d7f9612daad8d90b337fb2f8d5ccd2571
Le Thuy Vy <lethuyvy321@gmail.com>
2024-05-14 18:05:26 +0700

    Bug CreateSlice

---
+++ Redux/local-blog/src/pages/blog/blog.slice.ts
@@ -0,0 +1,107 @@
+import { createAction, createReducer, createSlice, current, nanoid } from '@reduxjs/toolkit'
+import { initialPostList } from 'constants/blog'
+import { Post } from 'types/blog.type'
+
+interface BlogState {
+  postList: Post[]
+  editingPost: Post | null
+}
+const initalState: BlogState = {
+  postList: initialPostList,
+  editingPost: null
+}
+
+export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
+  return {
+    payload: {
+      ...post,
+      id: nanoid()
+    }
+  }
+})
+export const deletePost = createAction<string>('blog/deletePost')
+export const startEditingPost = createAction<string>('blog/startEditingPost')
+export const cancelEditingPost = createAction('blog/cancelEditingPost')
+export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+const blogSlice = createSlice({
+  name : 'blog',
+  initialState 
+})
+const blogReducer = createReducer(initalState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      console.log('start', current(state))
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+      console.log('finish', current(state))
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const postId = action.payload
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    })
+    .addMatcher(
+      (action) => action.type.includes('cancel'),
+      (state, action) => {
+        console.log(current(state))
+      }
+    )
+})
+
+// const blogReducer = createReducer(initalState, {
+//   [addPost.type]: (state : any, action: PayloadAction<Post>) => {
+//     const post = action.payload
+//     state.postList.push(post)
+//   },
+//   [deletePost.type]: (state : any, action: any) => {
+//     console.log('start', current(state))
+//     const postId = action.payload
+//     const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+//     if (foundPostIndex !== -1) {
+//       state.postList.splice(foundPostIndex, 1)
+//     }
+//     console.log('finish', current(state))
+//   },
+//   [startEditingPost.type]: (state: any, action: any) => {
+//     const postId = action.payload
+//     const foundPost = state.postList.find((post) => post.id === postId) || null
+//     state.editingPost = foundPost
+//   },
+//   [cancelEditingPost.type]: (state: any) => {
+//     state.editingPost = null
+//   },
+//   [finishEditingPost.type]: (state: any, action: PayloadAction<Post>) => {
+//     const postId = action.payload.id
+//     state.postList.some((post, index) => {
+//       if (post.id === postId) {
+//         state.postList[index] = action.payload
+//         return true
+//       }
+//       return false
+//     })
+//     state.editingPost = null
+//   }
+// })
+
+export default blogReducer
